409fbeae5fff496f46de670e9bbe926731e4a36c
Jesús <jegico93@gmail.com>
2020-01-25 10:56:51 +0100

    Optimización layout, variables scss y botones

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  menuOpen: boolean;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  fireMenu() {
+    this.menuOpen = true;
+  }
 }
